30f765a3d1aa05479b910fd658ea48dc298368b2
Tian Xia <xiatian20@westlake.edu.cn>
2026-06-16 10:21:27 +0800

    Remove BuildArena project files.

--- buildarena/blocks/descriptors/torch.py
+++
@@ -1,8 +0,0 @@
-from besiege.build import Block
-from besiege.utils import format_float_array
-
-def descriptor(self: Block):
-    geo = self.geo
-    vec = geo.rotation.vec_abs.real
-    flame_pos = vec + self.center_pos.real
-    return f"Heating up a spherical area with radius 0.3 around {format_float_array(flame_pos)}"
